b7101a5ff56e8692847e4e08808fa1579ad36b73
DanyYap <124513296+DanyYap@users.noreply.github.com>
2025-05-24 14:56:58 +0800

    All_Model_Implement

--- Assets/Script/DialogueTrigger.cs
+++ Assets/Script/DialogueTrigger.cs
@@ -6,6 +6,10 @@ public class DialogueTrigger : MonoBehaviour
     public DialogueData dialogueData;
     public int lineIndex;
 
+    [Header("Optional Model Change")]
+    public bool changeModelOnTrigger = false;
+    public int newModelIndex; // 0: Baby, 1: Student, etc.
+
     private bool hasTriggered = false;
 
     private void OnTriggerEnter(Collider other)
@@ -17,6 +21,12 @@ public class DialogueTrigger : MonoBehaviour
             if (lineIndex >= 0 && lineIndex < dialogueData.lines.Length)
             {
                 DialogueManager.Instance.PlayLine(dialogueData.lines[lineIndex]);
+
+                if (changeModelOnTrigger)
+                {
+                    PlayerModelManager.Instance.SetModel(newModelIndex);
+                }
+
                 hasTriggered = true;
             }
             else
---
+++ Assets/Script/PlayerModelManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerModelManager : MonoBehaviour
+{
+    public static PlayerModelManager Instance;
+
+    [Header("Player Models in Order (Baby → OldMan)")]
+    public GameObject[] playerModels; // 0: Baby, 1: Student, 2: Adult, 3: OldMan
+
+    private int currentModelIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(gameObject);
+        else Instance = this;
+
+        SetModel(0); // Start with Baby
+    }
+
+    public void SetModel(int index)
+    {
+        if (index < 0 || index >= playerModels.Length) return;
+
+        // Disable all models
+        foreach (var model in playerModels)
+        {
+            if (model != null) model.SetActive(false);
+        }
+
+        // Enable the selected model
+        playerModels[index].SetActive(true);
+        currentModelIndex = index;
+    }
+
+    public int GetCurrentModelIndex() => currentModelIndex;
+}
